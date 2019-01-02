826129d07a4a2ce2d931162833efe83feff0b3d6
dzesszy738 <45570148+dzesszy738@users.noreply.github.com>
2019-01-02 17:40:03 +0100

    Nowa wersja

--- Logowanie2/Logowanie/Controllers/AccountController.cs
+++ Logowanie2/Logowanie/Controllers/AccountController.cs
@@ -19,8 +19,12 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Diagnostics;
 using System.Text;
+
+using Logowanie.Data;
+
 using System.Net.Mail;
 using System.Security.Cryptography;
+
 
 
 namespace Logowanie.Controllers
@@ -32,14 +36,25 @@ namespace Logowanie.Controllers
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly ILogger _logger;
 
+<<<<<<< HEAD
         
+
+=======
+
+      
+      
+        ApplicationDbContext _db;
+       
+>>>>>>> 9240a9fc30408071a944371fc6ab158c7edd3964
+
 
 
         public AccountController(
                     UserManager<ApplicationUser> userManager,
                     SignInManager<ApplicationUser> signInManager,
-                    ILogger<AccountController> logger)
+                    ILogger<AccountController> logger,ApplicationDbContext db)
         {
+            _db = db;
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
@@ -130,8 +145,9 @@ namespace Logowanie.Controllers
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Register(RegisterViewModel model, string returnUrl = null)
+        public async Task<IActionResult> Register(RegisterViewModel model, string returnUrl)
         {
+            returnUrl = null;
             ViewData["ReturnUrl"] = returnUrl;
             if (ModelState.IsValid)
             {
@@ -140,8 +156,13 @@ namespace Logowanie.Controllers
                 if (result.Succeeded)
                 {
                     await _userManager.AddToRoleAsync(user, "Pacjent");
-                 
+                    await _userManager.AddClaimAsync(user, new Claim(ClaimTypes.Role, "Pacjent"));
 
+                    Pacjent p = new Pacjent();
+                    p.Email = user.Email;
+                    _db.Add(p);
+
+                    _db.SaveChanges();
                     string ctoken = _userManager.GenerateEmailConfirmationTokenAsync(user).Result;
                     string ctokenlink = Url.Action("ConfirmEmail", "Account", new
                     {
@@ -149,8 +170,10 @@ namespace Logowanie.Controllers
                         token = ctoken
                     }, protocol: HttpContext.Request.Scheme);
                     ViewBag.token = ctokenlink;
+
                     string sub = "Weryfikacja adresu email";
                     Test1(user.Email, "W celu weryfikacji adresu email prosimy o kliknięcie w link: " + ctokenlink, sub);
+
 
                     await _signInManager.SignInAsync(user, isPersistent: false);
 
