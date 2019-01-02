ce6dbc0cd89e502f8bba65be0ffb5b0f35d7e68e
MarlenaMr <45628687+MarlenaMr@users.noreply.github.com>
2019-01-02 17:37:15 +0100

    Pacjent+Lekarz

---
+++ Logowanie2/Logowanie/Migrations/20181229135235_a.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Logowanie.Migrations
+{
+    public partial class a : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Opis",
+                table: "Wizyty",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Zalecenia",
+                table: "Wizyty",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Opis",
+                table: "Wizyty");
+
+            migrationBuilder.DropColumn(
+                name: "Zalecenia",
+                table: "Wizyty");
+        }
+    }
+}
---
+++ Logowanie2/Logowanie/Migrations/20181229140850_s.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Logowanie.Migrations
+{
+    public partial class s : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Nazwa",
+                table: "Leki",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Nazwa",
+                table: "Leki");
+        }
+    }
+}
